2040408ba2550a07c4e3e20a1b944231b2683045
Jack251970 <1160210343@qq.com>
2025-07-29 22:12:36 +0800

    Add new navigation view

---
+++ source/iNKORE.UI.WPF.Modern.Controls/Controls/Windows/SettingNavigationView/NavigationViewBackRequestedEventArgs.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
+
+namespace Flow.Bar.Controls.NavigationView
+{
+    public sealed class NavigationViewBackRequestedEventArgs : EventArgs
+    {
+        internal NavigationViewBackRequestedEventArgs()
+        {
+        }
+    }
+}
---
+++ source/iNKORE.UI.WPF.Modern.Controls/Controls/Windows/SettingNavigationView/NavigationViewItemInvokedEventArgs.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
+using iNKORE.UI.WPF.Modern.Media.Animation;
+
+namespace Flow.Bar.Controls.NavigationView
+{
+    public sealed class NavigationViewItemInvokedEventArgs : EventArgs
+    {
+        public NavigationViewItemInvokedEventArgs()
+        {
+        }
+
+        public object InvokedItem { get; internal set; }
+        public bool IsSettingsInvoked { get; internal set; }
+
+        public NavigationViewItemBase InvokedItemContainer { get; internal set; }
+        public NavigationTransitionInfo RecommendedNavigationTransitionInfo { get; internal set; }
+    }
+}
